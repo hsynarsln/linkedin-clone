3fb5031abf87b19c5e145d4a7fa4aa7b8f4bc4eb
hsynarslan <hsyn06arslan@gmail.com>
2021-12-28 11:05:04 +0300

    styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { Container, Content, Layout, Section } from '../styles/HomeStyles';
 import LeftSide from './LeftSide';
 import Main from './Main';
 import RightSide from './RightSide';
@@ -28,58 +28,6 @@ const Home = props => {
     </Container>
   );
 };
-
-const Container = styled.div`
-  padding-top: 52px;
-  max-width: 100%;
-`;
-
-const Content = styled.div`
-  max-width: 1128px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Section = styled.section`
-  min-height: 50px;
-  padding: 16px 0;
-  box-sizing: content-box;
-  text-align: center;
-  text-decoration: underline;
-  display: flex;
-  justify-content: center;
-  h5 {
-    color: #0a66c2;
-    font-size: 14px;
-    a {
-      font-weight: 700;
-    }
-  }
-  p {
-    font-size: 14px;
-    color: #434649;
-    font-weight: 600;
-  }
-  @media (max-width: 768px) {
-    flex-direction: column;
-    padding: 0 5px;
-  }
-`;
-
-const Layout = styled.div`
-  display: grid;
-  grid-template-areas: 'leftside main rightside';
-  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
-  column-gap: 25px;
-  row-gap: 25px;
-  /* grid-template-row: auto; */
-  margin: 25px 0;
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    padding: 0 5px;
-  }
-`;
 
 const mapStateToProps = state => {
   return {
