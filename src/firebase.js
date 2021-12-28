80cf3a2ef7199fc80c14915173448375d268f7c6
hsynarslan <hsyn06arslan@gmail.com>
2021-12-28 10:29:53 +0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCPvvq_w9GjF5mQBiNcuK0HmuYd3RPSGLA',
-  authDomain: 'linkedin-clone-14bf8.firebaseapp.com',
-  projectId: 'linkedin-clone-14bf8',
-  storageBucket: 'linkedin-clone-14bf8.appspot.com',
-  messagingSenderId: '252446701925',
-  appId: '1:252446701925:web:e65b9619dd74147504898c'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
